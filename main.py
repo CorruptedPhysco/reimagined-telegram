4a23511653ba0f3c596773c5afacf36578f68d72
CorruptedPhysco <106505409+CorruptedPhysco@users.noreply.github.com>
2024-03-14 11:36:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route("/")  # this sets the route to this page
+def home():
+	return "Hello! this is the main page <h1>HELLO</h1>"
+
+
